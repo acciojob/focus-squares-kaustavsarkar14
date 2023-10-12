91e300e9a4c89c68e79371433865b7af00b931b5
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-10-12 14:19:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll('.square')
+
+squares.forEach((el,i)=>{
+	el.addEventListener('mouseover',e=>{
+		squares.forEach(el=>{
+			if(el!=e.target){
+				el.style.backgroundColor = "#6F4E37"
+			}
+		})
+	})
+	el.addEventListener('mouseleave',e=>{
+		squares.forEach(el=>{
+			if(el!=e.target){
+				el.style.backgroundColor = "#E6E6FA"
+			}
+		})
+	})
+})
